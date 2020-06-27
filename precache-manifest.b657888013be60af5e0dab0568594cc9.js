0b6f0c22f1b8d982dc3001a5d3a526c8c8d698c9
brendagold <petersbrenda47@gmail.com>
2020-06-27 15:07:25 +0100

    Updates

---
+++ precache-manifest.b657888013be60af5e0dab0568594cc9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe78baa492c20d2503c4acd0f7f05bc",
+    "url": "https://brendagold.github.io/vidly/index.html"
+  },
+  {
+    "revision": "c4285431e63413eba913",
+    "url": "https://brendagold.github.io/vidly/static/css/2.e5ec43df.chunk.css"
+  },
+  {
+    "revision": "82715022592929f2f9a3",
+    "url": "https://brendagold.github.io/vidly/static/css/main.ddfc707c.chunk.css"
+  },
+  {
+    "revision": "c4285431e63413eba913",
+    "url": "https://brendagold.github.io/vidly/static/js/2.2c4206d6.chunk.js"
+  },
+  {
+    "revision": "afd58e6bf550033f94a96b5ca0ba61d2",
+    "url": "https://brendagold.github.io/vidly/static/js/2.2c4206d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82715022592929f2f9a3",
+    "url": "https://brendagold.github.io/vidly/static/js/main.f73e4eb7.chunk.js"
+  },
+  {
+    "revision": "7cd3ef3e870d8930efc4",
+    "url": "https://brendagold.github.io/vidly/static/js/runtime-main.1f4ffcc9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "https://brendagold.github.io/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "https://brendagold.github.io/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "https://brendagold.github.io/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "https://brendagold.github.io/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "https://brendagold.github.io/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
