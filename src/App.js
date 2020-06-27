d53901a563953bfe0fa402567a8836d81200c6c6
brendagold <petersbrenda47@gmail.com>
2020-06-27 18:48:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ class App extends Component {
       <React.Fragment>
         <NavBar />
         <main className="container">
+          <h1>Welcome To Vidly Movie Application</h1>
+          <p>Click Here to Start Application</p>
+          <Link to="/movies">Home Page</Link>
           <Switch>
             <Route path="/register" component={Register} />
             <Route path="/login" component={LoginForm} />
